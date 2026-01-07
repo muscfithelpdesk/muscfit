9418781ac723a7884ed59a79378aecf6cc374f9c
chaitanya sethi <chaitanyasethi3336@gmail.com>
2026-01-07 18:01:08 +0530

    style: increase logo size and detach from header height

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -194,12 +194,17 @@ export default function Header({ topOffset = 0, isFixed = true }) {
               </button>
             )}
 
-            <Link href="/" className="group flex items-center relative z-20 transition-transform duration-300 hover:scale-105">
-              <img
-                src="/assets/images/logo-v4.png"
-                alt="MUSCFIT Logo"
-                className={`transition-all duration-300 w-auto object-contain ${scrolled ? 'h-[50px] md:h-[60px]' : 'h-[60px] md:h-[130px]'}`}
-              />
+            <Link href="/" className="group relative z-20 block h-full aspect-[2/1] md:aspect-[3/1] flex-shrink-0">
+              <div className="absolute inset-0 flex items-center justify-start md:justify-center">
+                <img
+                  src="/assets/images/logo-v4.png"
+                  alt="MUSCFIT Logo"
+                  className={`transition-all duration-300 w-auto max-w-none object-contain ${scrolled
+                      ? 'h-[80px] md:h-[90px] mt-2'
+                      : 'h-[100px] md:h-[200px] mt-4'
+                    }`}
+                />
+              </div>
             </Link>
           </div>
 
